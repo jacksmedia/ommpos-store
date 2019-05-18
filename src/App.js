1422ecfe217e75b558fc1a2efef6f79774bdeeaa
alexanderjacks <alexanderthejacks@gmail.com>
2019-05-18 15:13:48 -0700

    :tada::art: 1st commit, incl custom favicon

--- src/App.js
+++ src/App.js
@@ -6,17 +6,17 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src={logo} className="App-logo pulse" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          How about we make this into a PWA?
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://www.alexanderjacks.info"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Built By AlexJacksApps
         </a>
       </header>
     </div>
