c726fc9684945f6e070540ed0b4c342c97c84f28
alexanderjacks <alexanderthejacks@gmail.com>
2019-05-19 16:23:35 -0700

    :sparkles::triangular_flag_on_post: service worker fully config'd for PWA UX

--- src/serviceWorker.js
+++ src/serviceWorker.js
@@ -51,6 +51,38 @@ export function register(config) {
         registerValidSW(swUrl, config);
       }
     });
+
+    var deferredPrompt;
+
+    window.addEventListener('beforeinstallprompt', (e) => {
+      // Prevent Chrome 67 and earlier from automatically showing the prompt
+      e.preventDefault();
+      // Stash the event so it can be triggered later.
+      deferredPrompt = e;
+      showAddToHomeScreen();
+    });
+
+    function showAddToHomeScreen() {
+      var a2hsButton = document.querySelector(".a2hs-prompt");
+      a2hsButton.style.display = "block";
+      a2hsButton.addEventListener("click", addToHomeScreen)
+    }
+
+    function addToHomeScreen() {
+      var a2hsButton = document.querySelector(".ad2hs-prompt");  // hide our user interface that shows our A2HS button
+      a2hsButton.style.display = 'none';  // Show the prompt
+      deferredPrompt.prompt();  // Wait for the user to respond to the prompt
+      deferredPrompt.userChoice
+        .then(function(choiceResult){
+      if (choiceResult.outcome === 'accepted') {
+        console.log('User accepted the A2HS prompt');
+      } else {
+        console.log('User dismissed the A2HS prompt');
+      }
+      deferredPrompt = null;
+      });
+    }
+
   }
 }
 
