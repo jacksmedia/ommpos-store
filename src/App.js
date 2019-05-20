c87178ecfdc0b8774f6150e0672d5de65067dfb1
alexanderjacks <alexanderthejacks@gmail.com>
2019-05-20 14:54:24 -0700

    :sparkles::construction: API response with all coins; doesnt build w/o breaking

--- src/App.js
+++ src/App.js
@@ -5,40 +5,49 @@ import './App.css';
 
 export default class App extends React.Component {
   
-  constructor(props) {   
+  constructor(props) {
     super(props)
     this.state = {
-        coins: [],
+      coins: []
     }
   }
 
-  componentDidMount() {
-        
-    console.log("t 3 s t");
-    
-    require('axios')
-      .get("https://api.nomics.com/v1/currencies/ticker?key=643698f1108812b938fe8a2d81983059&interval=1d,30d&quote-currency=USD")
-      .then(response => console.log(response))
+  componentDidMount() {      
+    console.log("m 0 u n t   $ u c c e $ $");
   }
 
   render(){
-      return (
-        <div className="App">
-          <header className="App-header">
-            <img src={logo} className="App-logo pulse" alt="logo" />
-            <p>
-              This PWA shiz is nontrivial.
-            </p>
-            <a
-              className="App-link"
-              href="https://www.alexanderjacks.info"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Built By AlexJacksApps
-            </a>
-          </header>
-        </div>
-      );
+    
+    axios.get("https://api.nomics.com/v1/prices?key=643698f1108812b938fe8a2d81983059&interval=1d,30d&quote-currency=USD")
+      .then(res => {
+        const coins = res.data;
+        this.setState({coins})
+      })
+
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo pulse" alt="logo" />
+          <p>
+            Here's an API call that only works in Dev, breaks on <code>yarn build</code>
+          </p>
+          <a
+            className="App-link"
+            href="https://www.alexanderjacks.info"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Built By AlexJacksApps
+          </a>
+        </header>
+        <section>
+          <ul>
+            { this.state.coins.map(
+              coin => <li>{coin.currency}</li>
+            )}
+          </ul>
+        </section>
+      </div>
+    );
   }
 }
