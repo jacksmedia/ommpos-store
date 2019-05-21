3f89ba432e4f45d6664a21de04fb61db2171f78f
alexanderjacks <alexanderthejacks@gmail.com>
2019-05-21 14:36:00 -0700

    :construction::tada: adapting over boilerplate begins

--- src/App.js
+++ src/App.js
@@ -1,35 +1,22 @@
 import React from 'react';
-import axios from 'axios';
 import logo from './logo.svg';
 import './App.css';
 
 export default class App extends React.Component {
   
-  constructor(props) {
-    super(props)
-    this.state = {
-      coins: []
-    }
-  }
-
+  
   componentDidMount() {      
-    console.log("m 0 u n t   $ u c c e $ $");
+    console.log("@ p p   m 0 u n t   $ u c c e $ $");
   }
 
   render(){
-    
-    axios.get("https://api.nomics.com/v1/prices?key=643698f1108812b938fe8a2d81983059&interval=1d,30d&quote-currency=USD")
-      .then(res => {
-        const coins = res.data;
-        this.setState({coins})
-      })
 
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo pulse" alt="logo" />
           <p>
-            Here's an API call that only works in Dev, breaks on <code>yarn build</code>
+            Gotta look out for myselves first.
           </p>
           <a
             className="App-link"
@@ -41,11 +28,8 @@ export default class App extends React.Component {
           </a>
         </header>
         <section>
-          <ul>
-            { this.state.coins.map(
-              coin => <li>{coin.currency}</li>
-            )}
-          </ul>
+         <h1>Primates need their chiefs! Stay celerbritory!
+         </h1>
         </section>
       </div>
     );
