fcee3142cb4038691054ff98cda98ebe6191cfa8
alexanderjacks <alexanderthejacks@gmail.com>
2019-07-06 19:20:32 -0700

    :art: more image assets

--- src/SortyTable.js
+++ src/SortyTable.js
@@ -5,10 +5,51 @@ import stuff from './stuff.json';
 
 export default function SortyTable() {
 
-	const JSONcolumns = ["Name", "ImageSrc", "When", "Found", "BaseSellPrice"];
+	const JSONcolumns = [
+	{
+		name: "Name",
+		label: "Name of Item",
+		options: {
+		   filter: true,
+		   sort: true,
+  		}
+	},
+	{
+		name: "ImageSrc",
+		label: "Image raw filename",
+		options: {
+		   filter: true,
+		   sort: false,
+  		}
+	},
+	{
+		name: "When",
+		label: "Season",
+		options: {
+		   filter: true,
+		   sort: true,
+  		}
+	},
+	{
+		name: "Found",
+		label: "Acquired by",
+		options: {
+		   filter: true,
+		   sort: true,
+  		}
+	},
+	{
+		name: "BaseSellPrice",
+		label: "BaseSellPrice",
+		options: {
+		   filter: true,
+		   sort: true,
+  		}
+	}
+	];
 	const JSONdata = stuff;
 	const JSONoptions = {
-	  filterType: 'checkbox',
+	  filterType: 'textField',
 	};
 
 	return(
